b557a229ea701b6e90d60d4a59d6066222a2d33e
Tomas Orobio <orobiotomas@gmail.com>
2021-11-15 00:42:36 -0300

    Creados Metodos Leer Postulantes y Admins

--- clases/Postulante.java
+++ clases/Postulante.java
@@ -3,11 +3,23 @@ package clases;
 import java.util.Date;
 import java.util.ArrayList;
 
-public class Postulante {
+
+public class Postulante extends Usuario {
     Date fechaNacimiento;
     ArrayList<Nacionalidad> nacionalidades;
     ArrayList<Idioma> idiomas;
     ArrayList<Interes> intereses;
     ArrayList<Publicacion> publicacionesFavoritas;
+
+    public Postulante(int dni,String nombre,String apellido,String mail, String contrasena,Date fechaNacimiento){
+        super(dni,nombre,apellido,mail,contrasena);
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void abrirMenu(){
+        System.out.println("logueado como: Postulante");
+        MenuEmpleador.mostrarMenu();
+        
+    }
 
 }
